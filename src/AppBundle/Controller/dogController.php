59718e241c842eb4f91adcdfe705b330b8e991b3
Marco Todaro <lacugurra@gmail.com>
2016-08-23 15:51:46 +0200

    upload profile  image

--- src/AppBundle/Controller/dogController.php
+++ src/AppBundle/Controller/dogController.php
@@ -10,31 +10,114 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\contact;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Csrf\CsrfExtension;
+use Symfony\Component\Security\Csrf\TokenStorage\SessionTokenStorage;
+use Symfony\Component\Security\Csrf\TokenGenerator\UriSafeTokenGenerator;
+use Symfony\Component\Security\Csrf\CsrfTokenManager;
+use Symfony\Component\Form\Forms;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class dogController extends Controller
 {
 
     /**
-     * @Route("/dog/index")
+     * @Route("/index" )
      */
 
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('dog/index.html.twig');
+        
+        $contact = new Contact();
+        $form = $this-> createFormbuilder($contact)
 
+            ->add('name', TextType::Class, array('attr'=> array('class'=> 'form-control','style'=> 'margin-bottom: 15px')))
+            ->add('email', TextType::Class, array('attr'=> array('class'=> 'form-control','style'=> 'margin-bottom: 15px')))
+            ->add('telephone', TextType::Class, array('attr'=> array('class'=> 'form-control','style'=> 'margin-bottom: 15px')))
+            ->add('message', TextareaType::Class, array('attr'=> array('class'=> 'form-control','style'=> 'margin-bottom: 15px')))
+             ->add('submit', SubmitType::Class, array('attr'=> array('class'=> 'btn btn-default','style'=> 'margin-bottom: 15px')))
+             ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $name = $form['name']->getData();
+            $email = $form['email']->getData();
+            $telephone = $form['telephone']->getData();
+            $message1 = $form['message']->getData();
+
+            $contact->setName($name);
+            $contact->setEmail($email);
+            $contact->setTelephone($telephone);
+            $contact->setMessage($message1);
+          
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($contact);
+            $em->flush();
+
+            $this->addFlash(
+
+                'notice',
+                'Contact Sent'
+            );
+
+
+            //sending the mail to the commerce
+             $message = \Swift_Message::newInstance()
+            ->setSubject('Hello '. $name. ' sent a contact request')
+            ->setFrom($email)
+            ->setTo('lacugurra@gmail.com')
+            ->setBody( $message1, 'text/html');
+            $this->get('mailer')->send($message);
+
+            return $this->render('dog/index.html.twig',array('form'=>$form->createView()));
+        }
+
+        return $this->render('dog/index.html.twig',array('form'=>$form->createView()));
+
+    }
+
+
+    /**
+     * @Route("/dog/list", name= "dog_list"  ) 
+     */
+
+    public function contactAction()
+    {
+        //render the contact request 
+        $contacts = $this->getDoctrine()->getRepository('AppBundle:contact')->findAll();
+        
+        return $this->render('dog/list.html.twig', array(
+            'contacts'=> $contacts
+        ));
     }
 
     /**
-     * @Route("/dog/signUp")
+     * @Route("/admin/", name= "admin"  ) 
      */
 
-    public function signUpAction()
+    public function adminAction()
     {
-
-        return $this->render('dog/signUp.html.twig');
-
+        return $this->render('dog/admin.html.twig');
     }
+
+    /**
+     * @Route("/profile/", name= "profile"  ) 
+     */
+
+    public function profileAction()
+    {
+        return $this->render('dog/profile.html.twig');
+    }
+
 
 }
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,248 @@
+<?php
+
+namespace AppBundle\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\File;
+
+/**
+ * User
+ * @Vich\Uploadable
+ * @ORM\Table(name="user_")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    // ..... other fields
+
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     * 
+     * @Vich\UploadableField(mapping="product_image", fileNameProperty="imageName")
+     * 
+     * @var File
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @var string
+     */
+    private $imageName;
+
+    /**
+     * @ORM\Column(type="datetime")
+     *
+     * @var \DateTime
+     */
+    private $updatedAt;
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the  update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $image
+     *
+     * @return Product
+     */
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        if ($image) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTime('now');
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return File|null
+     */
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    /**
+     * @param string $imageName
+     *
+     * @return Product
+     */
+    public function setImageName($imageName)
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getImageName()
+    {
+        return $this->imageName;
+    }
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="surname", type="string", length=255)
+     */
+    private $surname;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="telephone", type="string", length=255)
+     */
+    private $telephone;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="activationDate", type="datetime")
+     */
+    private $activationDate;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set surname
+     *
+     * @param string $surname
+     *
+     * @return User
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    /**
+     * Get surname
+     *
+     * @return string
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+   
+    /**
+     * Set telephone
+     *
+     * @param string $telephone
+     *
+     * @return User
+     */
+    public function setTelephone($telephone)
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    /**
+     * Get telephone
+     *
+     * @return string
+     */
+    public function getTelephone()
+    {
+        return $this->telephone;
+    }
+
+    /**
+     * Set activationDate
+     *
+     * @param \DateTime $activationDate
+     *
+     * @return User
+     */
+    public function setActivationDate($activationDate)
+    {
+        $this->activationDate = new \DateTime('now'); 
+
+        return $this;
+    }
+
+    /**
+     * Get activationDate
+     *
+     * @return \DateTime
+     */
+    public function getActivationDate()
+    {
+        return $this->activationDate;
+    }
+
+    
+
+    
+}
+
